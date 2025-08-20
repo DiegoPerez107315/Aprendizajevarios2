5657de59962526f8c8e8b0b24404f48abaf2f4d0
Diego Zambrano <dp1073150812@gmail.com>
2025-08-19 20:34:53 -0500

    simplificacion funcion

--- matriz adjunta.py
+++ matriz adjunta.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-matriz = np.array([(5,3,-2),(-1,4,0),(4,2,1)])
+matriz = np.array([[6, 1, -3], [-1, 5, 2], [7, -4, 7]])
 signos = np.array([[(-1) ** (i + j) for j in range(3)] for i in range(3)])
 matriz2 = np.array([(5,3),(-1,4)])
 
@@ -9,39 +9,18 @@ def determinante (mat):
 
 #Transforma matriz de 3*3 a 2*2 
 
-def trans (mat,fila,columna):
-    if fila== 0 and  columna == 0:
-        return np.array([(mat[1,1], mat[1,2]), (mat[2,1], mat[2,2])])
-    elif fila == 0 and columna == 1:
-        return np.array([(mat[1,0], mat[1,2]), (mat[2,0], mat[2,2])])
-    elif fila == 0 and columna ==2:
-        return np.array([(mat[1,0], mat[1,1]), (mat[2,0], mat[2,1])])
-    elif fila == 1 and columna == 0:
-        return np.array([(mat[0,1], mat[0,2]), (mat[2,1], mat[2,2])])
-    elif fila == 1 and columna == 1:
-        return np.array([(mat[0,0], mat[0,2]), (mat[2,0], mat[2,2])])
-    elif fila == 1 and columna == 2:
-        return np.array([(mat[0,0], mat[0,1]), (mat[2,0], mat[2,1])])
-    elif fila == 2 and columna == 0:
-        return np.array([(mat[0,1], mat[0,2]), (mat[1,1], mat[1,2])])
-    elif fila == 2 and columna == 1:
-        return np.array([(mat[0,0], mat[0,2]), (mat[1,0], mat[1,2])])
-    elif fila == 2 and columna == 2:
-        return np.array([(mat[0,0], mat[0,1]), (mat[1,0], mat[1,1])])
+def trans(mat, fila, columna):
+    # Crear índices para filas y columnas excluyendo la fila y columna especificadas
+    filas = [i for i in range(3) if i != fila]
+    columnas = [j for j in range(3) if j != columna]
+    return mat[np.ix_(filas, columnas)]
 
 def adj (mat) :
     adj_de_mat = np.array([(0,0,0),(0,0,0),(0,0,0)])
     for i in range(0,3):
         for j in range(0,3):
             adj_de_mat[i,j]=int(determinante(trans(mat,i,j)))
-    return adj_de_mat*signos
-
-
-
-
-            
-
-    
+    return (adj_de_mat*signos).T
 
 print(adj(matriz))
 
